bbc521605c01741b3def0bb0fad97ae22767952b
Ming-Jan (Randy) Pan <randy19981223@gmail.com>
2023-11-20 22:24:29 -0500

    Periodic

--- mjpan507/settings.py
+++ mjpan507/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#s9+7qduvsot0y!57^7w%az0!ykdk81v!@%c0&!s++z013jb-y'
+#! deprecated secret key
+# SECRET_KEY = 'django-insecure-#s9+7qduvsot0y!57^7w%az0!ykdk81v!@%c0&!s++z013jb-y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
