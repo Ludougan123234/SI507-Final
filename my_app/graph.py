2ee492a3211a7f0e1fd0c43ee2c26c9a312a3788
Ming-Jan (Randy) Pan <randy19981223@gmail.com>
2023-11-23 18:03:25 -0500

    CKPT

--- my_app/graph.py
+++ my_app/graph.py
@@ -1,22 +1,28 @@
-"""Contains definitions for graphs, vertex, and edge"""
+"""Definition for graph, edge, and vertex"""
 
 class Edge:
-    def __init__(self, src, dest, weight=0, additional_info=None):
-        self.src = src
-        self.dest = dest
-        self.weight = weight
-        self.additional_info = additional_info
+    def __init__(self, src, dest, source, severity, additional_info):
+        self.src = src # source
+        self.dest = dest # destination 
+        self.source = source  # source of info
+        self.severity = severity  # severity of interaction
+        self.additional_info = additional_info  # description of interaction
 
     def __str__(self):
-        return f"Edge from {self.src.id} to {self.dest.id} with weight {self.weight} and info {self.additional_info}"
+        return f"{self.src.id} and {self.dest.id} has interaction \"{self.additional_info}\"\n"
 
 class Vertex:
-    def __init__(self, value):
-        self.id = value
+    def __init__(self, value, name):
+        self.id = value # RxNorm CUI
+        # TODO: Name
+        self.name = name
+        # TODO: User choices - implemented patient sex agg. only
+        # self.reaction_sex = reaction_sex
+
         self.connectedTo = {}  # Key: Vertex, Value: Edge
 
     def __str__(self):
-        return f"{self.id} is connected to {', '.join([str(edge) for edge in self.connectedTo.values()])}"
+        return f"{''.join([str(edge) for edge in self.connectedTo.values()])}"
 
     def add_neighbor(self, nbr, edge):
         self.connectedTo[nbr] = edge
@@ -26,20 +32,40 @@ class Graph:
         self.vert_list = {}  # vertices
         self.num_vertices = 0
 
-    def add_vertex(self, key):
+    def add_vertex(self, key, name):
         self.num_vertices += 1
-        new_vertex = Vertex(key)
+        new_vertex = Vertex(key, name)
         self.vert_list[key] = new_vertex
         return new_vertex
 
-    def add_edge(self, f, t, weight=0, additional_info=None):
+    def add_edge(self, f, f_name, t, t_name, source, severity, additional_info):
+        # vertex
         if f not in self.vert_list:
-            self.add_vertex(f)
+            self.add_vertex(f, f_name)
         if t not in self.vert_list:
-            self.add_vertex(t)
-        
-        edge = Edge(self.vert_list[f], self.vert_list[t], weight, additional_info)
+            self.add_vertex(t, t_name)
+
+        # edge
+        edge = Edge(self.vert_list[f], self.vert_list[t], source, severity, additional_info)
         self.vert_list[f].add_neighbor(self.vert_list[t], edge)
-        # undirected graph
-        reverse_edge = Edge(self.vert_list[t], self.vert_list[f], weight, additional_info)
+        # If the graph is undirected, you might also want to add the reverse edge:
+        reverse_edge = Edge(self.vert_list[t], self.vert_list[f], source, severity, additional_info)
         self.vert_list[t].add_neighbor(self.vert_list[f], reverse_edge)
+
+# Example usage
+graph = Graph()
+
+
+for i in content['fullInteractionTypeGroup']:
+    for j in i['fullInteractionType']:
+        graph.add_edge(
+            j['minConcept'][0]['rxcui'],
+            j['minConcept'][0]['name'],
+            j['minConcept'][1]['rxcui'],
+            j['minConcept'][1]['name'],
+            i['sourceName'],
+            j['interactionPair'][0]['severity'],
+            j['interactionPair'][0]['description']
+        )
+
+print(graph.vert_list["207106"])
