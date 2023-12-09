f3ec3073c88b110dd488aff6c0ec8ae17ef7a470
Ming-Jan (Randy) Pan <randy19981223@gmail.com>
2023-12-09 14:44:29 -0500

    Fix plot migrate issue

--- mjpan507/settings.py
+++ mjpan507/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,5 @@ PLOTLY_COMPONENTS = [
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+django_heroku.settings(locals())
